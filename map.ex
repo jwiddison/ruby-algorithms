647b72cc6bd87f0313c0ea044c08f433d635c318
Jordan Widdison <jordan.widdison@gmail.com>
2019-04-14 14:47:28 -0600

    Get the elixir map implementations working

--- map.ex
+++ map.ex
@@ -1,17 +1,47 @@
+# Can run this file with the following:
+#
+#   iex> mix run map.ex --no-mix-exs
+#
+
 defmodule Maps do
   def iterative_map(list, func) do
-    new_list = []
-    Enum.each(list, fn(item) -> new_list <> func.(item) end)
-    new_list
+    # Can't be done iteratively. I tried everything, but couldn't colect
+    # the results from Enum.each/2 into a List to return.
+    Enum.map(list, &(func.(&1)))
   end
 
   def recursive_map(list, func) do
     case list do
       [item] -> [func.(item)]
-      [item | rest] -> [func.(item), recursive_map(rest, func)]
+      [item | rest] ->
+        List.flatten([func.(item), recursive_map(rest, func)])
     end
   end
-  def recursive_map(_list, []), do: nil
+
+  def reduced_map(list, func) do
+    # NOTE: Could also be written like this:
+    #
+    #   Enum.reduce(list, [], &(&2 ++ [func.(&1)]))
+    #
+    # NOTE: Or like this:
+    #
+    #   list
+    #   |> Enum.reduce([], fn i, acc -> [func.(i) | acc] end)
+    #   |> Enum.reverse
+    Enum.reduce(list, [], fn i, acc -> acc ++ [func.(i)] end)
+  end
 end
 
-Maps.iterative_map([1,2,3,4,5], fn(i) -> i * 2 end)
+defmodule MapsCaller do
+  def call do
+    IO.puts("Iterative Map:")
+    Maps.iterative_map([1,2,3,4,5], fn i -> i * 2 end) |> IO.inspect()
+    IO.puts("Recursive Map:")
+    Maps.recursive_map([1,2,3,4,5], fn i -> i * 2 end) |> IO.inspect()
+    IO.puts("Reduced Map:")
+    # NOTE: The &(&1 * 2) is shorthand for the anonymous function
+    Maps.reduced_map([1,2,3,4,5], &(&1 * 2)) |> IO.inspect()
+  end
+end
+
+MapsCaller.call()
